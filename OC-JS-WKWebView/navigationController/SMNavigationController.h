d387b981c310a71ae5607bed76ad4c5fbaaa61ab
lee <leeronchi@aliyun.com>
2019-10-14 22:28:08 +0800

    Create SMNavigationController.h

---
+++ OC-JS-WKWebView/navigationController/SMNavigationController.h
@@ -0,0 +1,17 @@
+//
+//  SMNavigationController.h
+//  OC-JS-WKWebView
+//
+//  Created by KT-yzx on 2019/10/14.
+//  Copyright © 2019 OC-JS-WKWebView. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SMNavigationController : UINavigationController
+
+@end
+
+NS_ASSUME_NONNULL_END

c2d42414ce715a4d54b89509a5f0ce49ae45aa53
75902641 <75902641@qq.com>
2019-10-14 13:29:04 +0800

    Create SMNavigationController.m

---
+++ OC-JS-WKWebView/SMNavigationController.m
@@ -0,0 +1,34 @@
+//
+//  SMNavigationController.m
+//  OC-JS-WKWebView
+//
+//  Created by KT-yzx on 2019/10/14.
+//  Copyright © 2019 OC-JS-WKWebView. All rights reserved.
+//
+
+#import "SMNavigationController.h"
+
+@interface SMNavigationController ()
+
+@end
+
+@implementation SMNavigationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

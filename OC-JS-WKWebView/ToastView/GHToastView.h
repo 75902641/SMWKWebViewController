65acfcf7d9f9bcbca571afeefe75426ec81815de
水淼淼 <75902641@qq.com>
2019-10-29 11:26:54 +0800

    Create GHToastView.h

---
+++ OC-JS-WKWebView/ToastView/GHToastView.h
@@ -0,0 +1,15 @@
+//
+//  GHToastView.h
+//  HealthCareO2OForDemander
+//
+//  Created by GongHe on 15/9/21.
+//  Copyright (c) 2015年 vodone.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GHToastView : UIView
+
++(void)showMessage:(NSString *)message;
+
+@end
